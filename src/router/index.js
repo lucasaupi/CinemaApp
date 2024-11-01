e8fc12154c034958413f5bab0f7fe1f4df88a764
juanparove <juanparove@gmail.com>
2024-11-01 10:24:45 -0300

    se agrego el ruteo y dependencias

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Login from '../components/login.vue';
 import HomePage from '../views/HomePage.vue';
+import MovieList from '../components/MovieList.vue';
+import SeatReservation from '../components/SeatReservation.vue';
 
 const routes = [
 
@@ -13,6 +15,16 @@ const routes = [
     path: '/login',
     name: 'Login',
     component: Login,
+  },
+  { 
+    path: '/',
+    name: 'MovieList', 
+    component: MovieList 
+  },
+  { 
+    path: '/reservation/:id', 
+    name: 'SeatReservation', 
+    component: SeatReservation 
   },
 ];
 
