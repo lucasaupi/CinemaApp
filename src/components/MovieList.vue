a2bf5ef08c24f8baba320a9f1ef6a5fc72b63441
Lucas <lucas-aupi@hotmail.com>
2024-11-04 20:47:14 -0300

    change styles

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -4,7 +4,7 @@
       <p>Elegi tu película:</p>
     </div>
     <div>
-      <h2>Cartelera de Películas</h2>
+      <h2 id="movie-list-title">Cartelera de Películas</h2>
       <div class="movie-list">
         <div v-for="movie in movies" :key="movie.id" class="movie-item">
           <img :src="`https://image.tmdb.org/t/p/w200${movie.poster_path}`" :alt="movie.title" />
@@ -51,7 +51,13 @@
     display: grid;
     grid-template-columns: repeat(4, 1fr);
     gap: 20px;
-    color: whitesmoke;
+    color: rgb(197, 168, 109);
+  }
+  #movie-list-title {
+  color: rgba(255, 255, 255, 0.8);
+  font-size: 2rem;
+  margin-bottom: 20px;
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.6);
   }
   .movie-item {
     text-align: center;
@@ -61,4 +67,3 @@
     border-radius: 8px;
   }
   </style>
-  
