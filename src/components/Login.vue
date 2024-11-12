ca0f8df5e0b3d421dda81a4b3c00c8ad87e970a2
Maximiliano <maximilianoluissantos@gmail.com>
2024-11-11 21:59:15 -0300

    Login_api

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -13,24 +13,49 @@
                 </div>
                 <button type="submit" class="btn btn-primary btn-block">Login</button>
             </form>
+            <p v-if="errorMessage"> {{ errorMessage }} </p>
         </div>
     </div>
 </template>
 
 <script>
+import { errorMessages } from "vue/compiler-sfc";
+import { ref, onMounted } from 'vue';
+import { useRoute } from 'vue-router';
 import "../Styles/loginStyle.css";
+import axios from "axios";
 export default {
     name: 'Login',
     data() {
         return {
             email: '',
-            password: ''
+            password: '',
+            errorMessage: '',
         };
     },
     methods: {
-        handleLogin() {
-            console.log('Email:', this.email);
-            console.log('Password:', this.password);
+        async handleLogin() {
+            try{
+                const respuesta= await axios.get('https://672f6ea666e42ceaf15db204.mockapi.io/users/users',{
+                    params: {
+                        email: this.email,
+                        password: this.password
+                        
+                    }
+                    
+                });
+                console.log(respuesta)
+                const user = respuesta.data.find(u=> u.email === this.email && u.password === this.password);
+                if (user) {
+                    this.$router.push('/SeatReservation');
+
+                } else {
+                    this.errorMessage='Usuario o password incorrecto';
+                }
+            } catch (error){
+                this.errorMessage = 'Error de conexión';
+            }
+            
         }
     }
 }
